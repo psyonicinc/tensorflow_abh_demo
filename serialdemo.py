206966545f0e0e6b1826e32743fb5c5caf110a21
ocanath <ocanath@gmail.com>
2021-12-23 19:22:37 -0600

    working write-only finger position movement to the hand using python. very lightweight demo. works

--- serialdemo.py
+++ serialdemo.py
@@ -1,14 +1,24 @@
 import serial
 from abh_api_core import farr_to_barr
 import math
+import time
 
 ser = serial.Serial('COM3','460800', timeout = 1)
 
-#define fpos
 fpos = [15., 15., 15., 15., 15., -15.]
-
-msg = farr_to_barr(fpos)
-print(msg)
-ser.write(msg)
+try:
+	while 1:
+		
+		#fpos = [15., 15., 15., 15., 15., -15.]
+		for i in range(0, len(fpos)):
+			ft = time.time()*3 + i
+			fpos[i] = (.5*math.sin(ft)+.5)*45+15
+		fpos[5] = -fpos[5]
+		
+		msg = farr_to_barr(fpos)
+		ser.write(msg)
+		time.sleep(.001)
+except KeyboardInterrupt:
+	pass
 
 ser.close()
