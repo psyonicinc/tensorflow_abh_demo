9dea6712b5e858929255e713e5cecb23a4b18f75
ocanath <ocanath@gmail.com>
2021-12-26 16:25:47 -0600

    sped up framerate (kind of hilariously considerably) by changing number of hands and tracking confidence interval

--- cv_demo.py
+++ cv_demo.py
@@ -1,17 +1,31 @@
 import cv2
 import mediapipe as mp
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+tprev = cv2.getTickCount()
+
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
+		max_num_hands=1,
 		model_complexity=0,
 		min_detection_confidence=0.5,
-		min_tracking_confidence=0.5) as hands:
+		min_tracking_confidence=0.33) as hands:
+		
+	fpos = [15,15,15,15,15,-15]
 	while cap.isOpened():
+	
+		ts = cv2.getTickCount()
+		tdif = ts-tprev
+		tprev = ts
+		fps = cv2.getTickFrequency()/tdif
+		
 		success, image = cap.read()
+		
 		if not success:
 			print("Ignoring empty camera frame.")
 			# If loading a video, use 'break' instead of 'continue'.
@@ -20,12 +34,14 @@ with mp_hands.Hands(
 		# To improve performance, optionally mark the image as not writeable to
 		# pass by reference.
 		image.flags.writeable = False
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		
+		
 		results = hands.process(image)
-
+		
+		
 		# Draw the hand annotations on the image.
-		image.flags.writeable = True
-		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		#image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 		if results.multi_hand_landmarks:
 			for hand_landmarks in results.multi_hand_landmarks:
 				mp_drawing.draw_landmarks(
@@ -36,6 +52,10 @@ with mp_hands.Hands(
 					mp_drawing_styles.get_default_hand_connections_style())
 		# Flip the image horizontally for a selfie-view display.
 		cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
 		if cv2.waitKey(5) & 0xFF == 27:
 			break
+		
+		#print (fps)
+		
 cap.release()
