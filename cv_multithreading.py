285f3d8609e276e1063cf11993e73463260ecee0
Jesse Cornman <ocanath@gmail.com>
2022-01-03 15:56:13 -0600

    added simpler kill  thread and removed try-catch for keyboard interrupt that does not work due to some unseen foolery with threading.

--- cv_multithreading.py
+++ cv_multithreading.py
@@ -27,8 +27,6 @@ exit = 0
 processing_done = 1
 image_ready = 0
 image = []
-abh = []
-results = []
 
 
 """
@@ -41,8 +39,6 @@ def process_thread():
 	global processing_done
 	global image_ready
 	global image
-	global abh
-	global results
 
 	""" 
 		Find a serial com port.
@@ -110,8 +106,6 @@ def image_thread():
 	global processing_done
 	global image_ready
 	global image
-	global abh
-	global results
 
 	# For webcam input:
 	cap = cv2.VideoCapture(0)
@@ -123,103 +117,63 @@ def image_thread():
 	warr_fps = [0,0,0]
 	lpf_fps_sos = signal.iirfilter(2, Wn=0.7, btype='lowpass', analog=False, ftype='butter', output='sos', fs=30)	#filter for the fps counter
 
-	try:
-		while not exit:
-				
-				
-				#fps counter
-				ts = cv2.getTickCount()
-				tdif = ts-tprev
-				tprev = ts
-				fps = cv2.getTickFrequency()/tdif
+	
+	while not exit:
+			
+			
+			#fps counter
+			ts = cv2.getTickCount()
+			tdif = ts-tprev
+			tprev = ts
+			fps = cv2.getTickFrequency()/tdif
 
-				#filter and print fps count
-				fpsfilt, warr_fps = py_sos_iir(fps, warr_fps, lpf_fps_sos[0])
-				print (fpsfilt)
+			#filter and print fps count
+			fpsfilt, warr_fps = py_sos_iir(fps, warr_fps, lpf_fps_sos[0])
+			print (fpsfilt)
 
 
-				#get new frame
-				success, image2 = cap.read()
-				if not success:
-					print("Ignoring empty camera frame.")
-					exit = 1
-					image_ready = 1
-					# If loading a video, use 'break' instead of 'continue'.
-					continue
-
-				#spin with timeout to wait for processing to finish
-				tstart = cv2.getTickCount()
-				while processing_done == 0:
-					t = (cv2.getTickCount()-tstart)/cv2.getTickFrequency()
-					if(t > 1.1):
-						break;
-
-				image=image2	#report image out to the process thread. double buffer needed to prevent race condition
-				processing_done = 0		#image is a 'master' thread. process is a 'slave'. We clear processing flag just before we allow it to run again
+			#get new frame
+			success, image2 = cap.read()
+			if not success:
+				print("Ignoring empty camera frame.")
+				exit = 1
 				image_ready = 1
-				
-	except KeyboardInterrupt:
-		print("Stopping")
-		exit = 1
-		image_ready = 1
+				# If loading a video, use 'break' instead of 'continue'.
+				continue
+
+			#spin with timeout to wait for processing to finish
+			tstart = cv2.getTickCount()
+			while processing_done == 0:
+				t = (cv2.getTickCount()-tstart)/cv2.getTickFrequency()
+				if(t > 1.1):
+					break;
+
+			image=image2	#report image out to the process thread. double buffer needed to prevent race condition
+			processing_done = 0		#image is a 'master' thread. process is a 'slave'. We clear processing flag just before we allow it to run again
+			image_ready = 1
+	
+	exit = 1
+	image_ready = 1
 	
 	cap.release()
 	print("Capture Stop")
 
-def display_thread():
+def kill_thread():
 	global exit
 	global processing_done
 	global image_ready
 	global image
-	global abh
-	global results
 
-	while not exit:
-		while image_ready == 0:
-			pass
-			
-		"""
-		if results:
-			if	results.multi_hand_landmarks:
-				#draw landmarks of the hand we found
-				hand_landmarks = results.multi_hand_landmarks[0]
-				mp_drawing.draw_landmarks(
-					image,
-					hand_landmarks,
-					mp_hands.HAND_CONNECTIONS,
-					mp_drawing_styles.get_default_hand_landmarks_style(),
-					mp_drawing_styles.get_default_hand_connections_style())
+	input()
+	exit = 1
 
-				#render a static point in the base frame of the model. Visualization of the position-orientation accuracy.
-				#Point should be just in front of the palm. Compensated for handedness
-				static_point_b = np.array([4.16, 1.05, -1.47*abh.handed_sign, 1])*abh.scale			
-				static_point_b[3] = 1	#remove scaling that was applied to the immutable '1'
-				neutral_thumb_w = abh.hw_b.dot(static_point_b)	#get dot position in world coordinates for a visual tag/reference				
-				l_list = landmark_pb2.NormalizedLandmarkList(
-					landmark = [
-						v4_to_landmark(neutral_thumb_w)
-					]
-				)
-				mp_drawing.draw_landmarks(
-					image,
-					l_list,
-					[],
-					mp_drawing_styles.get_default_hand_landmarks_style(),
-					mp_drawing_styles.get_default_hand_connections_style())
-		"""
-		
-		# Flip the image horizontally for a selfie-view display.
-		cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-		if cv2.waitKey(1) & 0xFF == 27:
-			exit = 1
-
-	print("Display Stop")
+	print("Exit signal sent")
 	
 if __name__ == "__main__":
 	
 	t1 = threading.Thread(target=process_thread, args=())
 	t2 = threading.Thread(target=image_thread,args=())
-	t3 = threading.Thread(target=display_thread, args=())
+	t3 = threading.Thread(target=kill_thread, args=())
 	
 	t1.start()
 	t2.start()
