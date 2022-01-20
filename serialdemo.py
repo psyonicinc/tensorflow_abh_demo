26d2a2cdb0b3fa02090e8750084d6b3f9a0d4af8
Jonathan <jtmulle2@illinois.edu>
2022-01-20 11:28:53 -0600

    334Hz Linux Functional

--- serialdemo.py
+++ serialdemo.py
@@ -1,6 +1,7 @@
 import serial
 from serial.tools import list_ports
 from abh_api_core import farr_to_barr
+from abh_api_core import create_misc_msg
 import math
 import time
 import ctypes
@@ -22,23 +23,26 @@ if not port:
 
 if(port):
 	try:
-		ser = serial.Serial("COM5",'460800', timeout = 0.002, inter_byte_timeout=0.0001)
+		ser = serial.Serial('/dev/ttyUSB0','460800', timeout = 0.005, inter_byte_timeout=0.001)
 		print ("connected!")
+		ser.reset_input_buffer()
 		buf = create_misc_msg(0xC2) # cmd to enable upsampling of the thumb rotator
 		ser.write(buf)
-	except:
+	except Exception as e:
 		port = ""
+		print(str(e))
 		print("failed to connect")
 
 
 fpos = [15., 15., 15., 15., 15., -15.]
 try:
-	start = time.process_time()
+	start = time.perf_counter()
 	print("Start at: " + str(start))
 	count = 0
 	timeouts = 0
 	badChecksums = 0
-	while count < 1000:
+	consecutiveChecksums = 0
+	while count < 100000:
 		count+=1
 		#print(str(count) +": ", end='')
 		#fpos = [15., 15., 15., 15., 15., -15.]
@@ -57,18 +61,22 @@ try:
 			if sum != 0:
 				print(str(count) +": " +"Check Sum Fail: " + str(sum))
 				badChecksums+=1
+				ser.reset_input_buffer()
 		else:
 			print(str(count) +": "+"Timeout!! "+ str(len(data)))
 			timeouts+=1
+			ser.reset_input_buffer()
 		
-		i = 0
+		#i = 0
 		#while i < 500000:
 		#	i += 1
 		#while time.process_time_ns() < (startTime + 50):
 		#	pass
-		time.sleep(0.001)
+	
+		#time.sleep(0.0004)
+
 		
-	end = time.process_time()
+	end = time.perf_counter()
 	elapsed = end - start
 	print("End at: " + str(end))
 	print("Total Number of Runs: " + str(count))
