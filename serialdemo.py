05531cff40216d546c7c607430edc5e94dd4ee43
ocanath <ocanath@gmail.com>
2022-11-05 11:50:29 -0700

    2 com port connections working

--- serialdemo.py
+++ serialdemo.py
@@ -1,49 +1,72 @@
 import serial
 from serial.tools import list_ports
-from abh_api_core import farr_to_barr
+from abh_api_core import *
 import math
 import time
+import numpy as np
 
 #ser = serial.Serial('COM4','460800', timeout = 1)
 """ 
 	Find a serial com port.
 """
 com_ports_list = list(list_ports.comports())
-port = ""
-ser = []
+port = []
+slist = []
 for p in com_ports_list:
 	if(p):
-		port = p
-		print("Found:", port)
-		break
+		pstr = ""
+		pstr = p
+		port.append(pstr)
+		print("Found:", pstr)
 if not port:
 	print("No port found")
 
-if(port):
+for p in port:
 	try:
-		ser = serial.Serial(port[0],'460800', timeout = 1)
-		print ("connected!")
-		buf = create_misc_msg(0xC2) # cmd to enable upsampling of the thumb rotator
-		ser.write(buf)
+		ser = []
+		ser = (serial.Serial(p[0],'460800', timeout = 1))
+		slist.append(ser)
+		print ("connected!", p)
+		# print ("found: ", p)
 	except:
-		port = ""
-		print("failed to connect")
+		print("failded.")
+		pass
+		# port = ""
+		# print("failed to connect")
+		
+		
 
+for s in slist:
+	buf = create_misc_msg(0xC2) # cmd to enable upsampling of the thumb rotator
+	print ("writing thumb filter message on com port: ", s)
+	s.write(buf)
 
-fpos = [15., 15., 15., 15., 15., -15.]
+fpos = [15., 15., 15., 15., 15., -15.]																									
 try:
 	while 1:
 		
-		#fpos = [15., 15., 15., 15., 15., -15.]
 		for i in range(0, len(fpos)):
-			ft = time.time()*3 + i
+			ft = time.time()*3 + i*(2*np.pi)/12
 			fpos[i] = (.5*math.sin(ft)+.5)*45+15
 		fpos[5] = -fpos[5]
 		
 		msg = farr_to_barr(fpos)
-		ser.write(msg)
+		slist[0].write(msg)
+
+
+		for i in range(0, len(fpos)):
+			ft = time.time()*3 + (i+6)*(2*np.pi)/12
+			fpos[i] = (.5*math.sin(ft)+.5)*45+15
+		fpos[5] = -fpos[5]
+		
+		msg = farr_to_barr(fpos)
+		slist[1].write(msg)
+	
 		time.sleep(.001)
+		
+		
 except KeyboardInterrupt:
 	pass
-
-ser.close()
+	
+for s in slist:
+	s.close()
