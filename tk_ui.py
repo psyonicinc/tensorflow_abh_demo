11cac52b0c0ffe5a3a6c2d007e8299f4708c1593
aihoque2 <aihoque2@illinois.edu>
2023-08-22 14:06:43 -0700

    tk ui has button binding to quit

--- tk_ui.py
+++ tk_ui.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
+
 window = tk.Tk()
+window.attributes('-fullscreen', True)
+
+def close_win(e):
+    window.destroy()
+window.bind('q', close_win)
+
 image = Image.open("default_img.jpg")
 photo_img_obj = ImageTk.PhotoImage(image)
 
+tk.Button(window, text="Quit", command=window.destroy).pack()
 
-canv = tk.Canvas(window, bg="white")
-canv.grid(row=2, column=3)
-canv.create_image(20, 20, anchor=tk.NW, image=photo_img_obj)
+canv = tk.Canvas(window, bg="white", highlightthickness=0)
+canv.pack(fill=tk.BOTH, expand=True)
+canv.create_image(0, 0, anchor=tk.NW, image=photo_img_obj)
 
 window.mainloop()
