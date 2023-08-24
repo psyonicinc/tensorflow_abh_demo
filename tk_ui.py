e7f03f73eae64747547efb9b4fbf51cf35a7b49f
aihoque2 <aihoque2@illinois.edu>
2023-08-24 14:25:38 -0700

    main method to test tk_ui

--- tk_ui.py
+++ tk_ui.py
@@ -270,7 +270,7 @@ class TKUI(tk.Tk):
 
             image = self.screen_saver
 
-            if (self.transition_count < float(self.fade_rate) and cap.isOpened()):
+            if (self.transition_count < float(self.fade_rate) and self.cap.isOpened()):
                 _, webcam_img = self.cap.read()
                 webcam_img = cv2.flip(webcam_img, 1)
                 webcam_img = cv2.resize(webcam_img, (self.dim[0], self.dim[1]), interpolation=cv2.INTER_CUBIC)
@@ -285,77 +285,17 @@ class TKUI(tk.Tk):
         self.label.photo_image = tk_img
         self.label.config(image=tk_img)
 
-window = tk.Tk()
-window.attributes('-fullscreen', True)
-window.config(cursor="none")
-image_label = tk.Label(window)
-image_label.pack()
 
-show_webcam = False
-wave_hand = True
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Hand CV Demo Parser')
+    parser.add_argument('--do_grip_cmds' , help="Include flag for using grip commands for grip recognitions", action='store_true')
+    parser.add_argument('--CP210x_only', help="for aadeel's bad computer", action='store_true')
+    parser.add_argument('--no_input', help="No input handler? Skip input reading step", action='store_true')
+    parser.add_argument('--reverse', help="reverse the order of the hands in order to map detections properly", action='store_true')
+    parser.add_argument('--camera_capture', type=int, help="opencv capture number", default=0)
+    parser.add_argument('--fade_rate', type=int, help="fade transition speed", default=20)
+    args = parser.parse_args()
 
-dim = pyautogui.size()
-
-# webcam input
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, fourcc)
-cap.set(cv2.CAP_PROP_FPS, 90)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(1920*720/1080))
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(1080*720/1080))
-
-
-if not cap.isOpened():
-    raise RuntimeError("cap.isOpened() returned false")
-
-# event handlers:
-def close_win(e):
-    window.destroy()
-
-def switch_img(e):
-    global show_webcam # Declare the global flag first before changing it
-    show_webcam = not show_webcam
-    print("here's flag: ", show_webcam)
-
-def switch_handwave(e):
-    global wave_hand
-    wave_hand = not wave_hand
-    print("here's wave_hand: ", wave_hand)
-
-window.bind('q', close_win)
-window.bind('a', switch_img)
-window.bind('x', switch_handwave)
-
-screen_saver = Image.open("default_img.jpg")
-screen_saver = screen_saver.resize((dim[0], dim[1]))
-screen_saver_img_obj = ImageTk.PhotoImage(screen_saver)
-
-
-def main_task():
-    global cap
-    global screen_saver_img_obj
-    if show_webcam:
-        success, frame = cap.read()
-        opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        captured_image = Image.fromarray(opencv_image).resize((dim[0], dim[1]))
-        photo_img = ImageTk.PhotoImage(image=captured_image)
-        image_label.photo_image = photo_img
-        image_label.config(image=photo_img)
-
-    else:
-        image_label.config(image=screen_saver_img_obj)
-        if wave_hand:
-            print("insert handwave here")
-        else:
-            print("hands would be still in this situation")
-    window.after(10, main_task)
-
-
-# tk.Button(window, text="Quit", command=window.destroy).pack()
-
-# canv = tk.Canvas(window, bg="white", highlightthickness=0)
-# canv.pack(fill=tk.BOTH, expand=True)
-# canv.create_image(0, 0, anchor=tk.NW, image=photo_img_obj)
-
-window.after(10, main_task)
-window.mainloop()
+    app = TKUI(use_grip_cmds=args.do_grip_cmds, CP210x_only=args.CP210x_only, no_input=args.no_input, reverse=args.reverse, camera_capture=args.camera_capture, fade_rate=args.fade_rate)
+    app.after(10, app.update)
+    app.mainloop()
