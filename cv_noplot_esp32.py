a0dcdb0a8b00dea7c666a882fb144c353585fb45
ocanath <ocanath@gmail.com>
2023-09-18 18:08:20 -0500

    autodetect and close if none detected

--- cv_noplot_esp32.py
+++ cv_noplot_esp32.py
@@ -24,7 +24,6 @@ if __name__ == "__main__":
 	parser.add_argument('--no_pinch_lock', help="disallow the grip/pinch locking", action='store_true')
 	parser.add_argument('--no_filter', help="remove lpf for raw", action='store_true')
 	parser.add_argument('--no_hose',help="refrain from sending hose activation command",action='store_true')
-	parser.add_argument('--num_hands',type=int,help="number of hands. min 1 max 2",default=1)
 	parser.add_argument('--sel_ip',help="manually select desired LAN IP if multiple network adapters are present",action='store_true')
 	args = parser.parse_args()
 	
@@ -33,193 +32,191 @@ if __name__ == "__main__":
 		print("Using grip commmands")
 	else:
 		print("Using hardloaded commands")
-
-	#number of hands
-	n = 1
-	if(args.num_hands >= 1 and args.num_hands <= 2):
-		n = args.num_hands
-
 	
 	addrs = []
 	#"left"
 	hand_port = 34345
-	string_address = locate_server_from_bkst_query(hand_port,kbrd_ip_select=args.sel_ip)[0]	
+	string_address,connected = locate_server_from_bkst_query(hand_port,kbrd_ip_select=args.sel_ip)
 	print("piping commands to: "+str(string_address)+" on port: "+str(hand_port))
 	udp_server_addr_tmp = (string_address,  hand_port)
-	addrs.append(udp_server_addr_tmp)
+	if(connected):
+		addrs.append(udp_server_addr_tmp)
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	client_socket.settimeout(0)
-	
-	if(n==2):
-		#"right"
-		hand_port = 23234
-		string_address = locate_server_from_bkst_query(hand_port,kbrd_ip_select=args.sel_ip)[0]
-		print("piping commands to: "+str(string_address)+" on port: "+str(hand_port))
-		udp_server_addr_tmp = (string_address,  hand_port)
-		addrs.append(udp_server_addr_tmp)
 		
+	#"right"
+	hand_port = 23234
+	string_address,connected = locate_server_from_bkst_query(hand_port,kbrd_ip_select=args.sel_ip)
+	print("piping commands to: "+str(string_address)+" on port: "+str(hand_port))
+	udp_server_addr_tmp = (string_address,  hand_port)
+	if(connected):
+		addrs.append(udp_server_addr_tmp)
+	
+	#number of hands
+	n = len(addrs)
 	
 	print("using "+str(len(addrs))+" sockets:")
 	for a in addrs:
 		print(a)
 	
+	if(n != 0):
 	
-	if(args.no_hose == False):
-		#note: if PPP stuffing is activated on the hand, this is likely unnecessary
-		hose_on_cmd = "activate_hose"
-		for addr in addrs:
-			print("sending command: "+hose_on_cmd+" to: "+str(addr[0])+":"+str(addr[1]))		
-			client_socket.sendto(bytearray(hose_on_cmd,encoding="utf8"),addr)
+		if(args.no_hose == False):
+			#note: if PPP stuffing is activated on the hand, this is likely unnecessary
+			hose_on_cmd = "activate_hose"
+			for addr in addrs:
+				print("sending command: "+hose_on_cmd+" to: "+str(addr[0])+":"+str(addr[1]))		
+				client_socket.sendto(bytearray(hose_on_cmd,encoding="utf8"),addr)
 
-	
-	lpf_fps_sos = signal.iirfilter(2, Wn=0.7, btype='lowpass', analog=False, ftype='butter', output='sos', fs=30)	#filter for the fps counter
-
-
-	prev_cmd_was_grip = [0,0]
-	
-	"""
-		Mediapipe setup/initialization
-	"""
-	mp_drawing = mp.solutions.drawing_utils
-	mp_drawing_styles = mp.solutions.drawing_styles
-	mp_hands = mp.solutions.hands
-
-	"""
-		Main program loop. 
 		
-		Does computer vision, angle extraction, filtering.
-		For plotting to work, execute as the 'frames' function.
-	"""
+		lpf_fps_sos = signal.iirfilter(2, Wn=0.7, btype='lowpass', analog=False, ftype='butter', output='sos', fs=30)	#filter for the fps counter
 
-	# For webcam input:
-	cap = cv2.VideoCapture(args.camera_capture)
-	cap.set(cv2.CAP_PROP_FPS, 90)
-	fps = int(cap.get(5))
-	#print("fps:",fps)
 
-	with mp_hands.Hands(
-			max_num_hands=n,
-			model_complexity=0,
-			min_detection_confidence=0.66,
-			min_tracking_confidence=0.66) as hands:
+		prev_cmd_was_grip = [0,0]
+		
+		"""
+			Mediapipe setup/initialization
+		"""
+		mp_drawing = mp.solutions.drawing_utils
+		mp_drawing_styles = mp.solutions.drawing_styles
+		mp_hands = mp.solutions.hands
+
+		"""
+			Main program loop. 
 			
-		tprev = cv2.getTickCount()	
+			Does computer vision, angle extraction, filtering.
+			For plotting to work, execute as the 'frames' function.
+		"""
+
+		# For webcam input:
+		cap = cv2.VideoCapture(args.camera_capture)
+		cap.set(cv2.CAP_PROP_FPS, 90)
+		fps = int(cap.get(5))
+		#print("fps:",fps)
+
+		with mp_hands.Hands(
+				max_num_hands=n,
+				model_complexity=0,
+				min_detection_confidence=0.66,
+				min_tracking_confidence=0.66) as hands:
 				
-		warr_fps = [0,0,0]
-		
-		#paramters for grip overload
-		abhlist = []
-		for i in range(0,n):
-			abh = AbilityHandBridge()
-			abh.lock_pinch = False
-			if(args.no_filter == True):
-				abh.filter_fpos = False
-			abhlist.append(abh)
-		
-		send_upsampling_msg_ts = 0
-		while cap.isOpened():
-		
-			ts = cv2.getTickCount()
-			tdif = ts-tprev
-			tprev = ts
-			fps = cv2.getTickFrequency()/tdif
+			tprev = cv2.getTickCount()	
+					
+			warr_fps = [0,0,0]
 			
-			success, image = cap.read()
+			#paramters for grip overload
+			abhlist = []
+			for i in range(0,n):
+				abh = AbilityHandBridge()
+				abh.lock_pinch = False
+				if(args.no_filter == True):
+					abh.filter_fpos = False
+				abhlist.append(abh)
 			
-			if not success:
-				print("Ignoring empty camera frame.")
-				# If loading a video, use 'break' instead of 'continue'.
-				continue
+			send_upsampling_msg_ts = 0
+			while cap.isOpened():
+			
+				ts = cv2.getTickCount()
+				tdif = ts-tprev
+				tprev = ts
+				fps = cv2.getTickFrequency()/tdif
+				
+				success, image = cap.read()
+				
+				if not success:
+					print("Ignoring empty camera frame.")
+					# If loading a video, use 'break' instead of 'continue'.
+					continue
 
-			# To improve performance, optionally mark the image as not writeable to
-			# pass by reference.
-			image.flags.writeable = False
-			
-			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+				# To improve performance, optionally mark the image as not writeable to
+				# pass by reference.
+				image.flags.writeable = False
+				
+				image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+							
+				results = hands.process(image)
+				
+
+				# Draw the hand annotations on the image.
+				image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+				
+				if results.multi_hand_landmarks:
+					
+					num_writes = 1
+					if(len(results.multi_hand_landmarks) == 2 and results.multi_handedness[0].classification[0].index != results.multi_handedness[1].classification[0].index):
+						num_writes = 2
+					for idx in range(0 , num_writes):
+						#log time for plotting
+						t = time.time()
+						ser_idx = results.multi_handedness[idx].classification[0].index	#can be 0 or 1
+						if(n == 1):
+							ser_idx = 0		#default to 0 if there's only one device connected
+						#args.no_pinch_lock
 						
-			results = hands.process(image)
-			
+						#fpos, warr, hw_b, hb_w, handed_sign, scale, dist_to_thumb = get_fpos(results, mp_hands, fpos, warr)
+						abhlist[ser_idx].update(mp_hands, results.multi_hand_landmarks[idx].landmark, ser_idx)
+						#if port:
+						
+						# Write the finger array out over UART to the hand!
+						msg = farr_to_barr(0x50, abhlist[ser_idx].fpos)
+						barr = bytearray(msg)
+						
+						#loopback. Server (subscriber) expectes straight up floating point with a 32 bit checksum. checksum eval not required
+						# dgram = bytearray(udp_pkt(abhlist[0].fpos))	#publish only 1 hand at a time in this context. 
+						# print("sending ", dgram)
+						client_socket.sendto(barr, addrs[ser_idx])
 
-			# Draw the hand annotations on the image.
-			image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+						#draw landmarks of the hand we found
+						hand_landmarks = results.multi_hand_landmarks[idx]
+						mp_drawing.draw_landmarks(
+							image,
+							hand_landmarks,
+							mp_hands.HAND_CONNECTIONS,
+							mp_drawing_styles.get_default_hand_landmarks_style(),
+							mp_drawing_styles.get_default_hand_connections_style())
 
-			
-			if results.multi_hand_landmarks:
+						#render a static point in the base frame of the model. Visualization of the position-orientation accuracy.
+						#Point should be just in front of the palm. Compensated for handedness
+						static_point_b = np.array([4.16, 1.05, -1.47*abhlist[ser_idx].handed_sign, 1])*abhlist[ser_idx].scale			
+						static_point_b[3] = 1	#remove scaling that was applied to the immutable '1'
+						neutral_thumb_w = abhlist[ser_idx].hw_b.dot(static_point_b)	#get dot position in world coordinates for a visual tag/reference				
+						l_list = landmark_pb2.NormalizedLandmarkList(
+							landmark = [
+								v4_to_landmark(neutral_thumb_w)
+							]
+						)
+						mp_drawing.draw_landmarks(
+							image,
+							l_list,
+							[],
+							mp_drawing_styles.get_default_hand_landmarks_style(),
+							mp_drawing_styles.get_default_hand_connections_style())
 				
-				num_writes = 1
-				if(len(results.multi_hand_landmarks) == 2 and results.multi_handedness[0].classification[0].index != results.multi_handedness[1].classification[0].index):
-					num_writes = 2
-				for idx in range(0 , num_writes):
-					#log time for plotting
-					t = time.time()
-					ser_idx = results.multi_handedness[idx].classification[0].index
-					if(n == 1):
-						ser_idx = 0		#default to 0 if there's only one device connected
-					#args.no_pinch_lock
 					
-					#fpos, warr, hw_b, hb_w, handed_sign, scale, dist_to_thumb = get_fpos(results, mp_hands, fpos, warr)
-					abhlist[ser_idx].update(mp_hands, results.multi_hand_landmarks[idx].landmark, ser_idx)
-					#if port:
-					
-					# Write the finger array out over UART to the hand!
-					msg = farr_to_barr(0x50, abhlist[ser_idx].fpos)
-					barr = bytearray(msg)
-					
-					#loopback. Server (subscriber) expectes straight up floating point with a 32 bit checksum. checksum eval not required
-					# dgram = bytearray(udp_pkt(abhlist[0].fpos))	#publish only 1 hand at a time in this context. 
-					# print("sending ", dgram)
-					client_socket.sendto(barr, addrs[ser_idx])
+				# Flip the image horizontally for a selfie-view display.
+				cv2.namedWindow('MediaPipe Hands', cv2.WINDOW_FREERATIO)
+				cv2.setWindowProperty('MediaPipe Hands',  cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_FREERATIO)
+				cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
 
-					#draw landmarks of the hand we found
-					hand_landmarks = results.multi_hand_landmarks[idx]
-					mp_drawing.draw_landmarks(
-						image,
-						hand_landmarks,
-						mp_hands.HAND_CONNECTIONS,
-						mp_drawing_styles.get_default_hand_landmarks_style(),
-						mp_drawing_styles.get_default_hand_connections_style())
+				if cv2.waitKey(1) & 0xFF == 27:
+					break
 
-					#render a static point in the base frame of the model. Visualization of the position-orientation accuracy.
-					#Point should be just in front of the palm. Compensated for handedness
-					static_point_b = np.array([4.16, 1.05, -1.47*abhlist[ser_idx].handed_sign, 1])*abhlist[ser_idx].scale			
-					static_point_b[3] = 1	#remove scaling that was applied to the immutable '1'
-					neutral_thumb_w = abhlist[ser_idx].hw_b.dot(static_point_b)	#get dot position in world coordinates for a visual tag/reference				
-					l_list = landmark_pb2.NormalizedLandmarkList(
-						landmark = [
-							v4_to_landmark(neutral_thumb_w)
-						]
-					)
-					mp_drawing.draw_landmarks(
-						image,
-						l_list,
-						[],
-						mp_drawing_styles.get_default_hand_landmarks_style(),
-						mp_drawing_styles.get_default_hand_connections_style())
-			
-				
-			# Flip the image horizontally for a selfie-view display.
-			cv2.namedWindow('MediaPipe Hands', cv2.WINDOW_FREERATIO)
-			cv2.setWindowProperty('MediaPipe Hands',  cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_FREERATIO)
-			cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-
-			if cv2.waitKey(1) & 0xFF == 27:
-				break
-
-			t_seconds = ts/cv2.getTickFrequency()
-			if(t_seconds > send_upsampling_msg_ts):
-				send_upsampling_msg_ts = t_seconds + 10
-				for i in range(0,n):
-					msg = create_misc_msg(0x50, 0xC2)
-					print("sending: ", [ hex(b) for b in msg ], "to ser device ", i)
-					barr = bytearray(msg)
-					client_socket.sendto(barr, addrs[i])
-					# slist[i].write(msg)
+				t_seconds = ts/cv2.getTickFrequency()
+				if(t_seconds > send_upsampling_msg_ts):
+					send_upsampling_msg_ts = t_seconds + 10
+					for i in range(0,n):
+						msg = create_misc_msg(0x50, 0xC2)
+						print("sending: ", [ hex(b) for b in msg ], "to ser device ", i)
+						barr = bytearray(msg)
+						client_socket.sendto(barr, addrs[i])
+						# slist[i].write(msg)
 
 
-			fpsfilt, warr_fps = py_sos_iir(fps, warr_fps, lpf_fps_sos[0])
-			# print (fpsfilt)
-	
-	for addr in addrs:
-		client_socket.sendto(bytearray("deactivate_hose",encoding="utf8"),addr)
-	
-	cap.release()
+				fpsfilt, warr_fps = py_sos_iir(fps, warr_fps, lpf_fps_sos[0])
+				# print (fpsfilt)
+		
+		for addr in addrs:
+			client_socket.sendto(bytearray("deactivate_hose",encoding="utf8"),addr)
+		
+		cap.release()
